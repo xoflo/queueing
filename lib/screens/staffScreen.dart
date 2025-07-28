afd0329778f07a3db7ecb819cbbfc7ea4eae2fad
xoflo <xordelish@gmail.com>
2025-07-28 23:16:39 +0800

    Revert "July 27"
    This reverts commit 6a22794e01ff6a681db91c7d6acfef806e835d07.

--- lib/screens/staffScreen.dart
+++ lib/screens/staffScreen.dart
@@ -443,7 +443,7 @@ class _StaffSessionState extends State<StaffSession> {
 
       print(type);
 
-      if (type == 'batchStatus' && json['status'] == 'denied') {
+      if (type == 'batchDenied') {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Action denied. Ticket already taken.")));
       }
 
